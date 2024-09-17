2b6ed304600a3e36cae4164485e34e50462ab29d
HenriqueSanti <henriquesanti.dev@gmail.com>
2024-09-17 17:32:35 -0400

    FST-Desktop version beta 01

---
+++ FST2.0/main.js
@@ -0,0 +1,35 @@
+//Navigation
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav ul li a");
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute("id");
+
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach(link => {
+                link.classList.remove("active");
+
+            });
+            document.querySelector(`header nav ul li a[href='#${id}']`).classList.add("active")
+        }
+    })
+}
+
+
+//Accordions
+
+const lines = document.querySelectorAll('.line');
+
+lines.forEach(line => {
+    line.addEventListener('click',  () => {
+        const body = line.querySelector('.cordeon-body');
+        body.classList.toggle('active');
+
+        const header = line.querySelector('.cordeon-header');
+        header.classList.toggle('active')
+    })
+})
